3fc20b8c20046569fc78095817e54a233d92872c
Marcus Lundgren <>
2018-02-21 13:49:03 +0100

    Fungerar detta?

--- BirthdayApp/ViewController.swift
+++ BirthdayApp/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         print("Test")
+        print("Secound test")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
